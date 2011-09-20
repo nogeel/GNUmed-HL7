890338f57299eefaa6dc7e40c967e9893a7aef06
Jeffrey Leegon <jeffrey@ubuntu.(none)>
2011-09-20 16:56:18 -0500

    gnumed_hl7_importer.py now uses a configuration file rather than having the database parameters hard coded in the file. Reformatted other file.

--- gnumed_hl7_importer.py
+++ gnumed_hl7_importer.py
@@ -1,46 +1,51 @@
 __author__ = 'Jeffrey Leegon'
 
-import os
 import re
 import hl7
 import string
 import hl7_interface_to_gnumed
 
 
-
-
 def process_lab_message(message):
-   # print hl7.ishl7(message)
-    #print message
+    '''Processes and HL7 lab message. Pulls out the necessary data for the clin.incoming_data_unmatched
+        into a dictionary along with the raw message then passes it on to the interface to be put in the database.
+    '''
+
     h = hl7.parse(message)
 
-
     temp = h.segment('PID')[2]
-    formatter_message ={'external_id': string.rstrip(str(temp[0]))}
-    formatter_message['last_name']  = h.segment('PID')[5][1]
+    formatter_message = {'external_id': string.rstrip(str(temp[0]))}
+    formatter_message['last_name'] = h.segment('PID')[5][1]
     formatter_message['first_name'] = h.segment('PID')[5][0]
-    temp =  h.segment('PID')[8]
-    formatter_message['gender'] =  string.rstrip(str(temp[0]))
+    temp = h.segment('PID')[8]
+    formatter_message['gender'] = string.rstrip(str(temp[0]))
     formatter_message['data_type'] = 'HL7'
     formatter_message['ordering_provider_information'] = " ".join(h.segment('OBR')[16])
     formatter_message['data'] = message
 
-    #print formatter_message
     hl7_interface_to_gnumed.insert_unmatched_record(formatter_message)
 
 
-file = open('test_data.hl7')
+def gnumed_hl7_importer(file_name):
+    file = open(file_name)
 
-message = ''
+    message = ''
 
-find_message_header = re.compile(r"^MSH")
+    find_message_header = re.compile(r"^MSH")
 
-for n in file:
-    if re.match(find_message_header, n):
+    for n in file:
+        if re.match(find_message_header, n):
+            if message != '':
+                process_lab_message(message)
+            message = n + '\r'
+        else:
+            message = message + n + '\r'
 
-        if message != '':
-            process_lab_message(message)
-        message = n + '\r'
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print "Systax is python gnumed_hl7_importer.py hl7_file_path"
     else:
-        message = message + n + '\r'
-  
+        gnumed_hl7_importer(sys.argv[1])
--- hl7_xml_unwrapper.py
+++ hl7_xml_unwrapper.py
@@ -20,8 +20,7 @@ if len(sys.argv) == 3:
     move_files = True
 
 
-def process_xml_file(in_file, out_file,  move_file_path=''):
-
+def process_xml_file(in_file, out_file, move_file_path=''):
     try:
         e_tree = ElementTree.parse(in_file)
     except:
@@ -41,18 +40,19 @@ def process_xml_file(in_file, out_file,  move_file_path=''):
     try:
         with open(out_file, 'w') as hl7_file:
             for node in messages:
-               hl7_file.write(node.text)
+                hl7_file.write(node.text)
     except IOError:
         GNUMedErrorWriter.write_error("Could not open the file")
 
     try:
         if move_files:
-          shutil.move(in_file, move_file_path)
+            shutil.move(in_file, move_file_path)
         else:
             temp_name = in_file + ".px"
             shutil.move(in_file, temp_name)
     except IOError:
-        GNUMedErrorWriter.write_error("File could not be moved to the specified directory. filename will be changed instead.")
+        GNUMedErrorWriter.write_error(
+            "File could not be moved to the specified directory. filename will be changed instead.")
 
 
 #Search for XML files located in the specified directory
@@ -61,16 +61,16 @@ input_directory = sys.argv[1]
 hl7_directory = sys.argv[2]
 processed_file_dir = sys.argv[3]
 
-input_directory = os.path.join( input_directory, "*.xml")
+input_directory = os.path.join(input_directory, "*.xml")
 xml_files = glob.glob(input_directory)
 print input_directory
 print xml_files
 # Process each file.
 for n in xml_files:
     print "Processing file:" + n
-    file_name =  os.path.split(n)[1]
+    file_name = os.path.split(n)[1]
     file_wo_extension = os.path.splitext(file_name)[0]
-    hl7_path =  os.path.join(hl7_directory, file_wo_extension + ".hl7")
+    hl7_path = os.path.join(hl7_directory, file_wo_extension + ".hl7")
     if move_files:
         move_path = os.path.join(processed_file_dir, file_name)
         process_xml_file(n, hl7_path, move_path)
